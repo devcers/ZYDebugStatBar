d0e9477822c0c7a9bd4aa0cdb8e4be859c5d57a5
zhaoyang <lizhaoyang@zhanghetianxia.com>
2017-11-06 10:53:06 +0800

    init CrashStatBar Project

---
+++ CrashStatBar/CrashStatBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashStatBar
+//
+//  Created by zhaoyang on 2017/11/6.
+//  Copyright © 2017年 zhaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrashStatBar/CrashStatBarTests/CrashStatBarTests.m
@@ -0,0 +1,39 @@
+//
+//  CrashStatBarTests.m
+//  CrashStatBarTests
+//
+//  Created by zhaoyang on 2017/11/6.
+//  Copyright © 2017年 zhaoyang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrashStatBarTests : XCTestCase
+
+@end
+
+@implementation CrashStatBarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
